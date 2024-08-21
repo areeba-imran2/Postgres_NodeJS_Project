95d6aff65fe8faab2479e5d903be4bd74ba0ea46
areeba-imran2 <areebaimran.edu@gmail.com>
2024-08-21 18:43:29 +0500

    Add .env file for environment variables and update db.js

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const { Client } = require('pg');
 
 // Create a new client instance
 const client = new Client({
-  host: 'localhost', 
-  port: 5432,        
-  user: 'postgres', 
-  password: '5A69@Te7', 
-  database: 'mydb' 
+  host: process.env.DB_HOST, 
+  port: process.env.DB_PORT,        
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME
 });
 
 // Connect to the PostgreSQL database
